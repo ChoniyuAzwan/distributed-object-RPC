fe2a11a79c6dd02e93e790e160134b195e616bc1
Choniyu Azwan <choniyuazwan@gmail.com>
2018-03-06 18:16:22 +0700

    second

--- func.py
+++ func.py
@@ -4,6 +4,16 @@ def hello(name):
 	print("client calling...")
 	return "Hello, %s" %name
 
+def kali(x,y):
+	hasil = x*y
+	return "hasil kali = %s" %hasil
+
+def bagi(x,y):
+	x = float(x)
+	y = float(y)
+	hasil = x/y
+	return "hasil bagi = %s" %hasil
+
 # def cdir(dirname):
 # 	os.mkdir("/tmp/%s" %dirname)
 # 	print "Directory %s created" % dirname
--- zclient.py
+++ zclient.py
@@ -3,4 +3,7 @@ import zerorpc
 c = zerorpc.Client()
 c.connect("tcp://10.0.3.1:4244")
 print c.hello("RPC")
+print c.kali(2,3)
+print c.bagi(8,4)
+
 #c.cdir("data1")
